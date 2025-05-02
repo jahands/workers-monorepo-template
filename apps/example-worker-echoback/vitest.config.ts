a684c534a83a24dfaf38af3e50a5d395d1f18af7
Jacob Hands <jacob@jacobhands.com>
2025-05-02 13:28:10 -0500

    remove test-helpers

--- apps/example-worker-echoback/vitest.config.ts
+++ apps/example-worker-echoback/vitest.config.ts
@@ -2,15 +2,6 @@ import { defineWorkersProject } from '@cloudflare/vitest-pool-workers/config'
 
 export default defineWorkersProject({
 	test: {
-		setupFiles: [`${__dirname}/src/test/setup.ts`],
-		deps: {
-			optimizer: {
-				ssr: {
-					enabled: true,
-					include: [],
-				},
-			},
-		},
 		poolOptions: {
 			workers: {
 				wrangler: { configPath: `${__dirname}/wrangler.jsonc` },
--- packages/test-helpers/src/test.ts
+++
@@ -1,3 +0,0 @@
-// This file can only be used within tests
-
-export { resetFetchMocks } from './fetch-mock'
