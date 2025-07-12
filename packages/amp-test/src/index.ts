e244b93b9bbefb31632a3e48cf2a4247cf6ec23b
Jacob Hands <jacob@jacobhands.com>
2025-07-12 09:19:41 -0500

    Add test package for zod schemas

---
+++ packages/amp-test/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Example function.
+ *
+ * @example
+ *
+ * Use from other apps/packages:
+ *
+ * ```ts
+ * import { hello } from '@repo/amp-test'
+ *
+ * hello()
+ * ```
+ */
+export function hello() {
+	return 'Hello, world!'
+}
