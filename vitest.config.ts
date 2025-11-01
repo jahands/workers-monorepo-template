eaa97a62f5ea53f3d204c4519d1f89e238ba7792
Jacob Hands <jacob@jacobhands.com>
2025-11-01 07:59:55 -0500

    feat: convert turbo/generators into workspace package

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,13 @@ import { defineConfig } from 'vitest/config'
 import { glob } from '@repo/workspace-dependencies/zx'
 
 export default defineConfig(async () => {
-	// All vitest projects
-	const projectConfigPaths = await glob(['{apps,packages}/*/vitest.config{,.node}.ts'])
+	const cfgTs = 'vitest.config{,.node}.ts'
+
+	// all vitest projects
+	const projectConfigPaths = await glob([
+		`{apps,packages}/**/${cfgTs}`,
+		`turbo/generators/**/${cfgTs}`,
+	])
 
 	return {
 		test: {
